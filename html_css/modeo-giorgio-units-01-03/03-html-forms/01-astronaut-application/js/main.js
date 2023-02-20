78c45d7516df680dafc1dc6b314cc4fd680d2459
giorgio-modeo <75391952+giorgio-modeo@users.noreply.github.com>
2023-02-20 17:54:44 +0100

    a

--- html_css/modeo-giorgio-units-01-03/03-html-forms/01-astronaut-application/js/main.js
+++ html_css/modeo-giorgio-units-01-03/03-html-forms/01-astronaut-application/js/main.js
@@ -1,1 +1,47 @@
-let user= get
+let form = document.getElementById("form")
+form.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    let fname = document.getElementById("fname")
+    let mname = document.getElementById("mname")
+    let lname = document.getElementById("lname")
+    let miss = document.getElementById("mission")
+    let weight = document.getElementById("weight")
+    let mail = document.getElementById("mail")
+    
+    let age = document.getElementById("age")
+    let gender = document.getElementById("gender")
+    let hair = document.getElementById("hair")
+    let eye = document.getElementById("eye")
+    let bio = document.getElementById("bio")
+    
+
+
+    alert("funziona")
+
+    const headers = new Headers()
+    headers.append("Content-Type", "application/json")
+
+    const body = {
+    "first name": fname,
+    "middle name": mname,
+    "last name": lname,
+    "mission":miss,
+    "weight" :weight,
+    "mail":mail,
+    "age":age,
+    "gender":gender,
+    "hair":hair,
+    "eye":eye,
+    "bio":bio
+
+    }
+
+    const options = {
+    method: "POST",
+    headers,
+    mode: "cors",
+    body: JSON.stringify(body),
+    }
+
+    fetch("https://eogakxqek5vcrtb.m.pipedream.net", options)
+});
